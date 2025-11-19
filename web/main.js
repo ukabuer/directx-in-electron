8943ece11ffbf8373149df50da0ccb6880affb71
ukabuer <ukabuer@live.com>
2025-11-20 01:13:50 +0800

    1

--- web/main.js
+++ web/main.js
@@ -1,85 +1,68 @@
-const {
-  app,
-  BrowserWindow,
-  OffscreenSharedTexture,
-  ipcMain,
-  nativeImage,
-} = require("electron");
+const { app, BrowserWindow, sharedTexture } = require("electron");
 const path = require("path");
 const { endianness } = require("os");
 const { spawn } = require("child_process");
 
 const target = "build/my-renderer.exe";
-const capturedTextures = new Map();
-
-console.log(OffscreenSharedTexture);
 
 const startNewProcess = (hwnd) => {
-  const data = endianness() == "LE" ? hwnd.readInt32LE() : hwnd.readInt32BE();
-  const p = spawn(target, [data], {
-    cwd: process.cwd(),
-  });
-  p.stdout.on("data", (data) => {
-    console.log(`[native side] ${data}`);
-  });
-  p.stderr.on("data", (data) => {
-    console.log(`[native side] ${data}`);
-  });
+	const data = endianness() === "LE" ? hwnd.readInt32LE() : hwnd.readInt32BE();
+	const p = spawn(target, [data], {
+		cwd: process.cwd(),
+	});
+	p.stdout.on("data", (data) => {
+		console.log(`[native side] ${data}`);
+	});
+	p.stderr.on("data", (data) => {
+		console.log(`[native side] ${data}`);
+	});
 };
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"),
+		},
+	});
 
-  win.loadFile(path.join(__dirname, "index.html"));
-  win.webContents.setFrameRate(60);
-  win.on("ready-to-show", () => {
-    // const hwnd = win.getNativeWindowHandle();
-    // startNewProcess(hwnd);
-  });
+	win.loadFile(path.join(__dirname, "index.html"));
+	win.webContents.setFrameRate(60);
+	win.on("ready-to-show", () => {
+		return;
+		const imported = sharedTexture.importSharedTexture({
+			textureInfo: {
+				codedSize: {
+					width: 400,
+					height: 300,
+				},
+				pixelFormat: "rgba",
+				handle: {
+					ntHandle: new Buffer(),
+				},
+			},
+		});
 
-  const osr = new BrowserWindow({
-    width: 128,
-    height: 128,
-    show: false,
-    webPreferences: {
-      offscreen: {
-        useSharedTexture: true,
-      },
-    },
-  });
+		sharedTexture.sendSharedTexture({
+			frame: win.webContents.mainFrame,
+			importedSharedTexture: imported,
+		});
 
-  osr.webContents.setFrameRate(1);
-  osr.webContents.on("paint", (event) => {
-    const texture = event.texture;
-
-    if (!texture) {
-      console.error("No texture, GPU may be unavailable, skipping.");
-      return;
-    }
-
-    console.log(texture.textureInfo);
-
-    win.webContents.send("osr-texture", texture.textureInfo);
-  });
-
-  const osrPath = path.join(__dirname, "osr.html");
-  osr.loadFile(osrPath);
+		imported.release();
+	});
 };
 
 app.whenReady().then(() => {
-  createWindow();
+	// startNewProcess(hwnd);
 
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+	createWindow();
+
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+	if (process.platform !== "darwin") app.quit();
 });
--- web/preload.js
+++ web/preload.js
@@ -1,37 +1,19 @@
 const { sharedTexture } = require("electron");
-const { ipcRenderer, contextBridge } = require("electron/renderer");
+const { contextBridge } = require("electron/renderer");
 
-contextBridge.exposeInMainWorld("textures", {
-  onSharedTexture: (cb) => {
-    ipcRenderer.on("shared-texture", async (e, transfer) => {
-      // Step 5: Get the shared texture from the transfer
-      const imported = sharedTexture.finishTransferSharedTexture(transfer);
-      // Step 6: Let the renderer render using WebGPU
-      await cb(imported);
-
-      // Step 10: Release the shared texture with a callback
-      imported.release(() => {
-        // Step 11: When GPU command buffer is done, we can notify the main process to release
-        ipcRenderer.send("shared-texture-done", id);
-      });
-    });
-  },
-  verifyCapturedImage: (verify) => {
-    ipcRenderer.on("verify-captured-image", (e, images) => {
-      verify(images, (result) => {
-        ipcRenderer.send("verify-captured-image-done", result);
-      });
-    });
-  },
+contextBridge.exposeInMainWorld("onSharedTexture", (cb) => {
+	sharedTexture.setSharedTextureReceiver(({ importedSharedTexture }) => {
+		cb(importedSharedTexture);
+	});
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
+	const replaceText = (selector, text) => {
+		const element = document.getElementById(selector);
+		if (element) element.innerText = text;
+	};
 
-  for (const dependency of ["chrome", "node", "electron"]) {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
-  }
+	for (const dependency of ["chrome", "node", "electron"]) {
+		replaceText(`${dependency}-version`, process.versions[dependency]);
+	}
 });
